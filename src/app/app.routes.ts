a0e6ef11d8399fda113c975ad625bf71268a9fbb
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-01-02 23:40:32 +0100

    add popper

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Layout/login/login.component';
 import { SignUpComponent } from './Layout/sign-up/sign-up.component';
+import { HomeComponent } from './Layout/home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
         redirectTo: 'home',
         pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
         title: 'Home'
     },
     {

966fb9723a57155dc0600b5048d8e6b37318dbc4
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-01-12 23:25:07 +0100

    update

--- src/app/Layout/home/home.component.ts
+++ src/app/Layout/home/home.component.ts
@@ -1,20 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { CardComponent } from "../../Shared_Components/card/card.component";
 import { StateService } from '../../Services/State/state.service';
-import { NgClass, NgStyle } from '@angular/common';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent, NgClass, NgStyle ],
+  imports: [CardComponent, NgClass ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   public stateService: StateService = inject(StateService);
 
-  // cards = ['1','2','3','4','5','6']
-    // cards = ['1','2','3','4','5','6','7','8']
+  cards = ['1','2','3','4','5','6']
 
-  cards = ['1','2','3','4']
+  // cards = ['1','2','3','4']
 }
